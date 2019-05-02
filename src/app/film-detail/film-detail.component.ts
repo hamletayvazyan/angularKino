d1e652b92b938828d0f6a3018bcd04ad05cbeb48
Ham <ha@lucky-team.ru>
2019-05-02 18:58:04 +0400

    added tickets

--- src/app/film-detail/film-detail.component.ts
+++ src/app/film-detail/film-detail.component.ts
@@ -1,32 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Location } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
 import {FilmsService} from '../films/films.service';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
-  selector: 'app-film-detail',
-  templateUrl: './film-detail.component.html',
-  styleUrls: ['./film-detail.component.css']
+    selector: 'app-film-detail',
+    templateUrl: './film-detail.component.html',
+    styleUrls: ['./film-detail.component.css']
 })
 export class FilmDetailComponent implements OnInit {
 
-  film;
-  constructor(private route: ActivatedRoute,
-              private filmService: FilmsService,
-              private location: Location) { }
+    film;
+    rows = [
+        {row: [{key: 'row 1'}, {key: 1}, {key: 2}, {key: 3}, {key: 4}, {key: 5}, {key: 6}, {key: 7}, {key: 8}]},
+        {row: [{key: 'row 2'}, {key: 1}, {key: 2}, {key: 3}, {key: 4}, {key: 5}, {key: 6}, {key: 7}, {key: 8}]},
+        {row: [{key: 'row 3'}, {key: 1}, {key: 2}, {key: 3}, {key: 4}, {key: 5}, {key: 6}, {key: 7}, {key: 8}]},
+        {row: [{key: 'row 4'}, {key: 1}, {key: 2}, {key: 3}, {key: 4}, {key: 5}, {key: 6}, {key: 7}, {key: 8}]},
+        {row: [{key: 'row 5'}, {key: 1}, {key: 2}, {key: 3}, {key: 4}, {key: 5}, {key: 6}, {key: 7}, {key: 8}]},
+    ];
 
-  ngOnInit() {
-    this.getFilm();
-  }
+    constructor(private route: ActivatedRoute,
+                private filmService: FilmsService,
+                private location: Location) {
+    }
 
-  getFilm(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.filmService.getFilm(id)
-        .subscribe(hero => this.film = hero);
-  }
+    ngOnInit() {
+        this.getFilm();
+    }
 
-  goBack(): void {
-    this.location.back();
-  }
+    getFilm(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.filmService.getFilm(id)
+            .subscribe(hero => this.film = hero);
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
 
 }
